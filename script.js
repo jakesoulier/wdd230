1f43029fbeeed4ed104a6897f290b0b124c8d8f4
jakesoulier <jakesoulier@byui.edu>
2022-09-14 21:44:29 -0600

    homepage

---
+++ script.js
@@ -0,0 +1,8 @@
+// const date = new Date()
+// date.getTime()
+// // console.log(date)
+// document.getElementById('dateTime').innerHTML = date;
+var string = document.lastModified;
+console.log(string)
+var update = 'Last updated: ' + string
+document.getElementById('dateTime').innerHTML = update
