dc1409d4b643145e40ec5ada75d9fe7741eb2406
jakesoulier <jakesoulier@byui.edu>
2022-11-10 15:45:07 -0700

    lsn09

---
+++ chamber/js/directory.js
@@ -0,0 +1,27 @@
+const requestURL = "./data/data.json";
+
+fetch(requestURL)       
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const buzlist = jsonObject['businesses'];
+    buzlist.forEach(displayCard);
+    // buzlist.forEach(displayList);
+  });
+
+function displayCard(business) {
+    let card = document.querySelector("#cardview");
+    let div = document.createElement("div");
+    div.innerHTML = `img src="${business.imamgeURL}>
+    <p>${business.address} ${business.citystatezip}</p>
+    <p>${business.phone}</p>
+    <p><a href="${business.websiteURL}">${business.websiteURL}</a></p>`;
+    console.log(div)
+    card.appendChild(div);
+}
+
+// function displayList(business){
+
+// }
