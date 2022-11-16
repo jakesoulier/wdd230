36c339fdf5ae9a519fdaac7979d9a8acfd0deeb1
jakesoulier <jakesoulier@byui.edu>
2022-11-16 02:36:06 -0700

    9

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -8,7 +8,7 @@ fetch(requestURL)
     console.table(jsonObject);  // temporary checking for valid response and data parsing
     const buzlist = jsonObject['businesses'];
     buzlist.forEach(displayCard);
-    // buzlist.forEach(displayList);
+    buzlist.forEach(displayList);
   });
 
 function displayCard(business) {
@@ -23,6 +23,34 @@ function displayCard(business) {
     card.appendChild(div);
 }
 
-// function displayList(business){
+function displayList(business) {
+  let list = document.querySelector('#listview');
+  let table = document.createElement('table');
+  table.innerHTML = `<tr><th>${business.name}</th>
+  <th>${business.street} ${business.citystatezip}</th>
+  <th>${business.phone}</th>
+  <th><a href="${business.websiteURL}">${business.websiteURL}</a></th></tr>`;
+  list.appendChild(table);
+}
 
-// }
+function hideCard(){
+  var cardView = document.querySelector('#cardview');
+  // var button = document.querySelector('button');
+  if (cardView.style.display === "grid") {
+    cardView.style.display = "none";
+    // button.style.opacity = "0.5";  
+  } else {
+    cardView.style.display = "grid";
+  }
+}
+
+function hideList(){
+  var listView = document.querySelector('#listview');
+  // var button = document.querySelector('button');
+  if (listView.style.display === "grid") {
+    listView.style.display = "none";
+    // button.style.opacity = "0.5";  
+  } else {
+    listView.style.display = "grid";
+  }
+}
