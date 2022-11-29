1acfdfa8a52cf4676467a326f52b276b019bfe0d
jakesoulier <jakesoulier@byui.edu>
2022-11-29 15:54:45 -0700

    temple

---
+++ temple/js/script.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    console.log('yo');
+    var x = document.getElementById('navbar');
+    if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+}
+console.log('hey');
